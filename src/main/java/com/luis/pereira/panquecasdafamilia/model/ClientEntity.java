f645b517b7085abf281e2cbe40d14e67af173bbd
LuisBatistaPereira <luisantoniobatistapereira@gmail.com>
2020-06-30 19:18:05 -0300

    Finalização das Classes

--- src/main/java/com/luis/pereira/panquecasdafamilia/model/ClientEntity.java
+++ src/main/java/com/luis/pereira/panquecasdafamilia/model/ClientEntity.java
@@ -1,5 +1,33 @@
 package com.luis.pereira.panquecasdafamilia.model;
 
+import lombok.*;
+import org.springframework.stereotype.Component;
+import javax.persistence.*;
 
+@Component
+
+//Lombok
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@ToString
+
+//JPA
+@Entity
+@Table(name = "client")
 public class ClientEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_client")
+    private long id_client;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "houseNumber")
+    private int houseNumber;
+
 }
--- src/main/java/com/luis/pereira/panquecasdafamilia/model/PancakeEntity.java
+++ src/main/java/com/luis/pereira/panquecasdafamilia/model/PancakeEntity.java
@@ -1,5 +1,36 @@
 package com.luis.pereira.panquecasdafamilia.model;
 
+import lombok.*;
+import org.springframework.stereotype.Component;
+import javax.persistence.*;
+
+@Component
+
+//Lombok
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@ToString
+
+//JPA
+@Entity
+@Table(name = "pancake")
 public class PancakeEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pancake")
+    private long id_pancake;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "value")
+    private double value;
+
+    @Column(name = "amount")
+    private int amount;
+    
 }
